a80b231632933fe244d6d98929eb369d047fed7f
jfourdachon <jfourdachon@yahoo.com>
2021-03-06 08:25:59 +0100

    create shop stackNavigator

--- App.tsx
+++ App.tsx
@@ -1,28 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/combineReducers';
+import ShopStackNavigator from './navigation/ShopStackNavigator';
 
 const store = createStore(rootReducer);
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style='auto' />
-      </View>
+      <ShopStackNavigator />
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/shop/ProductsOverviewScreen.tsx
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { NavigationStackScreenComponent } from 'react-navigation-stack'
+import { useSelector } from 'react-redux'
+import { ROOT_STATE } from '../../store/combineReducers'
 
 
-const ProductsOverviewScreen = () => {
+const ProductsOverviewScreen: NavigationStackScreenComponent = () => {
+
+    const products = useSelector((state:ROOT_STATE) => state.products.availableProducts)
     return (
-        <View></View>
+        <FlatList data={products} keyExtractor={item => item.id} renderItem={itemData => <Text>{itemData.item.title}</Text>} />
     )
+}
+
+ProductsOverviewScreen.navigationOptions = () => {
+    return {
+        headerTitle: 'All Products',
+    }
 }
 
 export default ProductsOverviewScreen
