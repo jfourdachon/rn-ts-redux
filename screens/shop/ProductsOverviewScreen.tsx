f2590d17ae9fc241a5dcc25cd668895a8dc0e201
jfourdachon <jfourdachon@yahoo.com>
2021-03-06 08:59:02 +0100

    FlatList in product overview

--- screens/shop/ProductsOverviewScreen.tsx
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import { NavigationStackScreenComponent } from 'react-navigation-stack'
-import { useSelector } from 'react-redux'
-import { ROOT_STATE } from '../../store/combineReducers'
-
+import React from 'react';
+import { FlatList } from 'react-native';
+import { NavigationStackScreenComponent } from 'react-navigation-stack';
+import { useSelector } from 'react-redux';
+import ProductItem from '../../components/shop/ProductItem';
+import { ROOT_STATE } from '../../store/combineReducers';
 
 const ProductsOverviewScreen: NavigationStackScreenComponent = () => {
-
-    const products = useSelector((state:ROOT_STATE) => state.products.availableProducts)
-    return (
-        <FlatList data={products} keyExtractor={item => item.id} renderItem={itemData => <Text>{itemData.item.title}</Text>} />
-    )
-}
+  const products = useSelector((state: ROOT_STATE) => state.products.availableProducts);
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <ProductItem
+          title={itemData.item.title}
+          imageUrl={itemData.item.imageUrl}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
+    />
+  );
+};
 
 ProductsOverviewScreen.navigationOptions = () => {
-    return {
-        headerTitle: 'All Products',
-    }
-}
+  return {
+    headerTitle: 'All Products',
+  };
+};
 
-export default ProductsOverviewScreen
-
-const styles = StyleSheet.create({})
+export default ProductsOverviewScreen;
