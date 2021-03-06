759c1e2afb94b809f4d2e3c0d6cef2d1f83facaa
jfourdachon <jfourdachon@yahoo.com>
2021-03-06 07:29:02 +0100

    connect store to app

---
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+
+
+const ProductsOverviewScreen = () => {
+    return (
+        <View></View>
+    )
+}
+
+export default ProductsOverviewScreen
+
+const styles = StyleSheet.create({})
