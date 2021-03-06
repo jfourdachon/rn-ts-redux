33ddd6220e8f914a84c80b81e0892cb7ddcf9e04
jfourdachon <jfourdachon@yahoo.com>
2021-03-06 07:28:29 +0100

    set redux products

---
+++ store/reducers/products.reducer.ts
@@ -0,0 +1,24 @@
+import Product from '../../models/product'
+import PRODUCTS from '../../data/dummy-data'
+
+export type PRODUCT_STATE = {
+    availableProducts: Product[],
+    filteredMeals: Product[],
+    favoriteMeals: Product[]
+}
+
+const initialState = {
+    availableProducts: PRODUCTS,
+    userProducts: PRODUCTS.filter((product:Product) => product.id === 'u1')
+}
+
+export default (state = initialState, { type, payload }: any) => {
+    switch (type) {
+
+    case 'typeName':
+        return { ...state, ...payload }
+
+    default:
+        return state
+    }
+}
