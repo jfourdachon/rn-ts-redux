691363fca0d050cc48ace0096e303f69585407e2
jfourdachon <jfourdachon@yahoo.com>
2021-03-06 08:25:43 +0100

    ad combineReducer type

--- store/combineReducers/index.ts
+++ store/combineReducers/index.ts
@@ -1,5 +1,9 @@
 import {combineReducers} from 'redux'
-import productsReducer from '../reducers/products.reducer'
+import productsReducer, { PRODUCT_STATE } from '../reducers/products.reducer'
+
+export type ROOT_STATE = {
+    products: PRODUCT_STATE
+}
 
 const rootReducer = combineReducers({
     products: productsReducer
