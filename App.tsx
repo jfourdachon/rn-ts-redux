f254e63dcdb0537e8449d2c2bc54e117a88db23e
jfourdachon <jfourdachon@yahoo.com>
2021-03-09 01:00:38 +0100

    add redux devtools

--- App.tsx
+++ App.tsx
@@ -4,28 +4,35 @@ import { createStore } from 'redux';
 import { Provider, useStore } from 'react-redux';
 import rootReducer from './store/combineReducers';
 import ShopStackNavigator from './navigation/ShopStackNavigator';
-import AppLoading from 'expo-app-loading'
-import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+// composeWithDevTools only for development !!!!!!!!!!!!!!!!!!
+// const store = createStore(rootReducer, composeWithDevTools());
 const store = createStore(rootReducer);
 
-
 const fetchFonts = () => {
-    return Font.loadAsync({
-      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-    });
-  };
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
 export default function App() {
-    const [fontLoaded, setFontLoaded] = useState(false)
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-    if (!fontLoaded) {
-    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} onError={(err) => console.log({err})} />
-}
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log({ err })}
+      />
+    );
+  }
   return (
     <Provider store={store}>
       <ShopStackNavigator />
     </Provider>
   );
 }
-
