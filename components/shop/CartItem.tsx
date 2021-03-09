15ac1f9d7669e73957461bec18b2fc26299ca10f
jfourdachon <jfourdachon@yahoo.com>
2021-03-09 20:06:34 +0100

    cart Items list

---
+++ components/shop/CartItem.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Touchable from '../touchable/Touchable';
+import { Ionicons } from '@expo/vector-icons';
+
+type IProps = {
+  title: string;
+  quantity: number;
+  amount: number;
+  onRemove: () => void;
+};
+
+const CartItem = ({ title, quantity, amount, onRemove }: IProps) => {
+  return (
+    <View style={styles.cartItem}>
+      <View style={styles.itemdata}>
+        <Text style={styles.quantity}>{quantity} </Text><Text style={styles.text}>{title}</Text>
+      </View>
+      <View style={styles.itemdata}>
+        <Text style={styles.text}>${amount.toFixed(2)}</Text>
+        <Touchable useForeground onPress={onRemove} style={styles.deleteButton}>
+          <Ionicons name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'} size={23} color="red" />
+        </Touchable>
+      </View>
+    </View>
+  );
+};
+
+export default CartItem;
+
+const styles = StyleSheet.create({
+  cartItem: {
+    padding: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 20,
+  },
+  itemdata: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  quantity: {
+    fontFamily: 'open-sans',
+    color: '#888',
+    fontSize: 16,
+  },
+  text: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 16,
+  },
+  deleteButton: {
+    marginLeft: 20,
+  },
+});
--- screens/shop/CartScreen.tsx
+++ screens/shop/CartScreen.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import { useSelector } from 'react-redux';
+import CartItem from '../../components/shop/CartItem';
 import Colors from '../../constants/Colors';
 import { ROOT_STATE } from '../../store/combineReducers';
 
 const CartScreen = () => {
-
-const cartTotalAmount = useSelector((state: ROOT_STATE) => state.cart.totalAmount)
-const cartItems = useSelector((state: ROOT_STATE) => {
-    const transformedCartItems= []
-    for(const key in state.cart.items) {
-        transformedCartItems.push({
-            productId: key,
-            productTitle: state.cart.items[key].productTitle,
-            productPrice: state.cart.items[key].productPrice,
-            quantity: state.cart.items[key].quantity,
-            sum: state.cart.items[key].sum,
-        })
+  const cartTotalAmount = useSelector((state: ROOT_STATE) => state.cart.totalAmount);
+  const cartItems = useSelector((state: ROOT_STATE) => {
+    const transformedCartItems = [];
+    for (const key in state.cart.items) {
+      transformedCartItems.push({
+        productId: key,
+        productTitle: state.cart.items[key].productTitle,
+        productPrice: state.cart.items[key].productPrice,
+        quantity: state.cart.items[key].quantity,
+        sum: state.cart.items[key].sum,
+      });
     }
-    return transformedCartItems
-})
+    return transformedCartItems;
+  });
   return (
     <View style={styles.screen}>
       <View style={styles.summary}>
@@ -28,9 +28,18 @@ const cartItems = useSelector((state: ROOT_STATE) => {
         </Text>
         <Button title='Order Now' onPress={() => {}} color={Colors.accent} disabled={cartItems.length === 0} />
       </View>
-      <View>
-        <Text>Cart Items</Text>
-      </View>
+      <FlatList
+        data={cartItems}
+        keyExtractor={(item) => item.productId}
+        renderItem={(itemData) => (
+          <CartItem
+            title={itemData.item.productTitle}
+            quantity={itemData.item.quantity}
+            amount={itemData.item.sum}
+            onRemove={() => {}}
+          />
+        )}
+      />
     </View>
   );
 };
@@ -38,29 +47,29 @@ const cartItems = useSelector((state: ROOT_STATE) => {
 export default CartScreen;
 
 const styles = StyleSheet.create({
-    screen: {
-        marginVertical: 20
-    },
-    summary: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom: 20,
-        marginHorizontal: 15,
-        padding: 10,
-        shadowColor: 'black',
-        shadowOpacity: 0.3,
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: 'white',
-    },
-    summaryText: {
-        fontFamily: 'open-sans-bold',
-        fontSize: 18
-    },
-    amount: {
-        color: Colors.primary
-    }
+  screen: {
+    marginVertical: 20,
+  },
+  summary: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    marginHorizontal: 15,
+    padding: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+  },
+  summaryText: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 18,
+  },
+  amount: {
+    color: Colors.primary,
+  },
 });
