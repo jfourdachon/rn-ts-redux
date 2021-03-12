b59ffae1db31f665ceb5bda6d6f9e623cb6e90fc
jfourdachon <jfourdachon@yahoo.com>
2021-03-12 22:06:58 +0100

    populate editProductScreen

--- screens/user/EditProductScreen.tsx
+++ screens/user/EditProductScreen.tsx
@@ -1,25 +1,91 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { NavigationStackScreenComponent } from 'react-navigation-stack'
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Platform,
+} from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { NavigationStackScreenComponent } from 'react-navigation-stack';
+import { useSelector } from 'react-redux';
+import CustomHeaderButton from '../../components/UI/HeaderButton';
+import { ROOT_STATE } from '../../store/combineReducers';
 
-type IProps = {
+type IProps = {};
 
-}
+const EditProductScreen: NavigationStackScreenComponent = ({ navigation }) => {
+  const prodId = navigation.getParam('productId');
+  const editedProduct = useSelector((state: ROOT_STATE) =>
+    state.products.userProducts.find((product) => product.id === prodId)
+  );
 
-const EditProductScreen: NavigationStackScreenComponent = () => {
-    return (
-        <View>
-            <Text>The Edit product screen</Text>
+  const [title, setTitle] = useState(editedProduct ? editedProduct.title : '');
+  const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState(editedProduct ? editedProduct.description : '');
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput style={styles.input} value={title} onChange={(e) => setTitle(e.nativeEvent.text)} />
         </View>
-    )
-}
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Image URL</Text>
+          <TextInput style={styles.input} value={imageUrl} onChange={(e) => setImageUrl(e.nativeEvent.text)} />
+        </View>
+        {editedProduct ? null : (
+          <View style={styles.formControl}>
+            <Text style={styles.label}>Price</Text>
+            <TextInput style={styles.input} value={price} onChange={(e) => setPrice(e.nativeEvent.text)} />
+          </View>
+        )}
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput style={styles.input} value={description} onChange={(e) => setDescription(e.nativeEvent.text)} />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
 
-EditProductScreen.navigationOptions = ({navigation}) => {
-    const id = navigation.getParam('productId')
-    return {
-        headerTitle: id ? 'Edit you product' : 'Add a product'
-    }
-}
-export default EditProductScreen
+EditProductScreen.navigationOptions = ({ navigation }) => {
+  const id = navigation.getParam('productId');
+  return {
+    headerTitle: id ? 'Edit Product' : 'Add Product',
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title='Save'
+          iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
+          onPress={() => {
+            navigation.navigate('EditProduct');
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+export default EditProductScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  form: {
+    margin: 20,
+  },
+  formControl: {
+    width: '100%',
+  },
+  label: {
+    fontFamily: 'open-sans-bold',
+    marginVertical: 8,
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+});
