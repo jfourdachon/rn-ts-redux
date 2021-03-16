b823b9fb7c48d4c8326c22e6cdaf3c8ebad3b3d3
jfourdachon <jfourdachon@yahoo.com>
2021-03-16 19:27:49 +0100

    Input to component

---
+++ enums/keyboard.ts
@@ -0,0 +1,19 @@
+export enum AutoCapitalize {
+  Sentences = "sentences",
+  Words = "words",
+}
+
+export enum KeyboardType {
+  Default = "default",
+  NumberPad = "number-pad",
+  DecimalPad = "decimal-pad",
+  Numeric = "numeric",
+  Email = "email-address",
+  Phone = "phone-pad",
+}
+
+export enum ReturnKeyType {
+  Done = "done",
+  Go = "go",
+  Next = "next",
+}
