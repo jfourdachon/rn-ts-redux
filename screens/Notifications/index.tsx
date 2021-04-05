8954613fc374e52396ee007f91c3a4e2f0c4f9f6
jfourdachon <jfourdachon@yahoo.com>
2021-04-05 10:46:56 +0200

    expo get push token

--- screens/Notifications/index.tsx
+++ screens/Notifications/index.tsx
@@ -4,53 +4,55 @@ import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
 
 Notifications.setNotificationHandler({
-    handleNotification: async () => {
-        return {
-            shouldPlaySound: false,
-            shouldShowAlert: true,
-            shouldSetBadge: true
-        }
-    }
-})
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldShowAlert: true,
+      shouldSetBadge: true,
+    };
+  },
+});
 
 const NotificationsScreen = () => {
   const triggerNotifHandler = () => {
-    Notifications.scheduleNotificationAsync({
-      content: { title: 'My first local Notif', body: 'This is the first local notif we are sending!' },
-      trigger: { seconds: 3 },
-    });
+    // Notifications.scheduleNotificationAsync({
+    //   content: { title: 'My first local Notif', body: 'This is the first local notif we are sending!' },
+    //   trigger: { seconds: 3 },
+    // });
+  };
+
+  const getExpoNotificationsToken = async () => {
+    const permission = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+
+    if (permission.status !== 'granted') {
+      const askPermission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      if (askPermission.status !== 'granted') {
+        throw new Error('Permission not granted!');
+      }
+    }
+    const response = await Notifications.getExpoPushTokenAsync()
+    console.log(response.data)
   };
 
   useEffect(() => {
-    const checkPermission = async () => {
-      const permission = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-      let askPermission;
-      if (permission.status !== 'granted') {
-        askPermission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-      }
-      if (askPermission?.status !== 'granted') {
-        // User grants no permission for notifications
-        return;
-      }
-    };
-    checkPermission();
+    getExpoNotificationsToken().catch(err => console.log({err}))
   }, []);
 
-useEffect(() => {
-    const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(response => {
-        // Any code we want here, navigate / http requests ...
-        console.log({response})
-    })
+  useEffect(() => {
+    const backgroundSubscription = Notifications.addNotificationResponseReceivedListener((response) => {
+      // Any code we want here, navigate / http requests ...
+      console.log({ response });
+    });
 
-    const foregroundSubscription = Notifications.addNotificationReceivedListener(notification => {
-        // Any code we want here, navigate / http requests ...
-        console.log(notification)
-    })
+    const foregroundSubscription = Notifications.addNotificationReceivedListener((notification) => {
+      // Any code we want here, navigate / http requests ...
+      console.log(notification);
+    });
     return () => {
-        foregroundSubscription.remove()
-        backgroundSubscription.remove()
-    }
-},[])
+      foregroundSubscription.remove();
+      backgroundSubscription.remove();
+    };
+  }, []);
 
   return (
     <View style={styles.screen}>
