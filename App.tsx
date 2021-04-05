25be6cf7646388b94a96e38c42618572e9896fac
jfourdachon <jfourdachon@yahoo.com>
2021-04-05 18:01:17 +0200

    add push in foreground

--- App.tsx
+++ App.tsx
@@ -6,6 +6,18 @@ import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import ReduxThunk from 'redux-thunk'
 import AppNavigator from './navigation/AppNavigator';
+import * as Notifications  from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => {
+      return {
+        shouldPlaySound: false,
+        shouldShowAlert: true,
+        shouldSetBadge: true,
+      };
+    },
+  });
+  
 
 // composeWithDevTools only for development !!!!!!!!!!!!!!!!!!
 // const store = createStore(rootReducer, composeWithDevTools());
